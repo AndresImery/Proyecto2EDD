b0c6f4266a46180cfdff0002dd5ed7fc8e5be4cb
CarlosEGonR <115117568+CarlosEGonR@users.noreply.github.com>
2023-06-30 18:08:20 -0400

    testing Carlos

--- src/proyecto2eddandresimerycarlosgonzalez/Main.java
+++ src/proyecto2eddandresimerycarlosgonzalez/Main.java
@@ -21,22 +21,26 @@ public class Main {
         
         Habitacion habitacion1 = new Habitacion(12, "doble", 2, null);
         Habitacion habitacion2 = new Habitacion(14, "single", 1, null);
+        Habitacion habitacion3 = new Habitacion(14, "triple", 1, null);
         
-        
-        
-        Cliente cliente1 = new Cliente(12345, "Name", "LastName", "email@correo.com", "04242773364", "msculino");
+        Cliente cliente1 = new Cliente(26012655, "Carlos", "Gonzalez", "emailcarlos@correo.com", "02129857474", "no binario@#$$$$");
         Reserva reserva1 = new Reserva(cliente1, "doble", new Date(25), new Date(26));
         Cliente cliente2 = new Cliente(123456, "Andres", "Imery", "emailimery@correo.com", "04242563364", "Macho");
         Reserva reserva2 = new Reserva(cliente2, "single", new Date(20), new Date(21));
+        Cliente cliente3 = new Cliente(12345, "Pedro", "Gonzales", "emailpedro@correo.com", "02129857874", "hembra");
+        Reserva reserva3 = new Reserva(cliente3, "triple", new Date(30), new Date(45));
+        
         
         Estado estado1 = new Estado(habitacion1, cliente1, reserva1.getLlegada());
         habitacion1.setEstado(estado1);
         Estado estado2 = new Estado(habitacion2, cliente2, reserva2.getLlegada());
         habitacion2.setEstado(estado2);
+        Estado estado3 = new Estado(habitacion3, cliente3, reserva3.getLlegada());
+        habitacion3.setEstado(estado3);
         
         hash.addEstado(estado1);
         hash.addEstado(estado2);
-        
+        hash.addEstado(estado3);
         hash.printHash();
         
         hash.deleteEstado("Andres", "Imery");
@@ -48,8 +52,14 @@ public class Main {
         
         arbol.insertReserva(arbol.getRoot(), reserva1);
         arbol.insertReserva(arbol.getRoot(), reserva2);
-        
-        
+        arbol.insertReserva(arbol.getRoot(), reserva3);
+        arbol.printReservaInOrder( arbol.getRoot());
+        System.out.println("-----------");
+        arbol.deleteReserva(arbol.getRoot(), reserva3);
+        arbol.printReservaInOrder( arbol.getRoot());
+        System.out.println("-----------");
+        arbol.deleteReserva(arbol.getRoot(), reserva3);
+        arbol.printReservaInOrder( arbol.getRoot());
     }
     
 }
