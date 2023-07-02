084440cb0b680e044deaacc3efe366034b93db53
CarlosEGonR <115117568+CarlosEGonR@users.noreply.github.com>
2023-07-01 21:03:50 -0400

    Interfaz 2

--- src/proyecto2eddandresimerycarlosgonzalez/BuscarReserva.java
+++ src/proyecto2eddandresimerycarlosgonzalez/BuscarReserva.java
@@ -26,30 +26,107 @@ public class BuscarReserva extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTextField1 = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabelBNombre = new javax.swing.JLabel();
+        jLabelBApellido = new javax.swing.JLabel();
+        jLabelBEmail = new javax.swing.JLabel();
+        jLabelBGenero = new javax.swing.JLabel();
+        jLabelBNombre1 = new javax.swing.JLabel();
+        jLabelBNombre2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(612, 406));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jTextField1.setText("CEDULA");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        jPanel1.setMaximumSize(new java.awt.Dimension(612, 406));
+        jPanel1.setPreferredSize(new java.awt.Dimension(612, 406));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel4.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel4.setFont(new java.awt.Font("Cambria", 1, 36)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 102, 0));
+        jLabel4.setText("CHECK-IN HOTEL BOOKING");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 10, -1, -1));
+
+        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 102, 0));
+        jLabel2.setText("Ingrese le cedule de le persone:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 70, -1, -1));
+        jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 70, 120, -1));
+
+        jButton1.setText("Buscar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 120, -1, -1));
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 70, -1, -1));
+
+        jButton2.setText("Volver");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 370, -1, -1));
+
+        jLabelBNombre.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelBNombre.setForeground(new java.awt.Color(0, 0, 0));
+        jLabelBNombre.setOpaque(true);
+        jPanel1.add(jLabelBNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 130, 90, 30));
+
+        jLabelBApellido.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelBApellido.setForeground(new java.awt.Color(0, 0, 0));
+        jLabelBApellido.setOpaque(true);
+        jPanel1.add(jLabelBApellido, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 170, 120, 30));
+
+        jLabelBEmail.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelBEmail.setForeground(new java.awt.Color(0, 0, 0));
+        jLabelBEmail.setOpaque(true);
+        jPanel1.add(jLabelBEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 210, 290, 30));
+
+        jLabelBGenero.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelBGenero.setForeground(new java.awt.Color(0, 0, 0));
+        jLabelBGenero.setOpaque(true);
+        jPanel1.add(jLabelBGenero, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 250, 120, 30));
+
+        jLabelBNombre1.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelBNombre1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabelBNombre1.setOpaque(true);
+        jPanel1.add(jLabelBNombre1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 290, 90, 30));
+
+        jLabelBNombre2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelBNombre2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabelBNombre2.setOpaque(true);
+        jPanel1.add(jLabelBNombre2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 330, 90, 30));
+
+        jLabel3.setText("Nombre");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 140, -1, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyecto2eddandresimerycarlosgonzalez/fondorecepcion.jpg"))); // NOI18N
         jLabel1.setOpaque(true);
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -87,7 +164,19 @@ public class BuscarReserva extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabelBApellido;
+    private javax.swing.JLabel jLabelBEmail;
+    private javax.swing.JLabel jLabelBGenero;
+    private javax.swing.JLabel jLabelBNombre;
+    private javax.swing.JLabel jLabelBNombre1;
+    private javax.swing.JLabel jLabelBNombre2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
