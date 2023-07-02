2372cca0f07368a794fb3e6796daa3dcdaab2966
CarlosEGonR <115117568+CarlosEGonR@users.noreply.github.com>
2023-07-01 22:16:27 -0400

    nomasporfa

--- src/proyecto2eddandresimerycarlosgonzalez/BuscarReserva.java
+++ src/proyecto2eddandresimerycarlosgonzalez/BuscarReserva.java
@@ -27,89 +27,106 @@ public class BuscarReserva extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         jLabelBNombre = new javax.swing.JLabel();
         jLabelBApellido = new javax.swing.JLabel();
         jLabelBEmail = new javax.swing.JLabel();
         jLabelBGenero = new javax.swing.JLabel();
         jLabelBNombre1 = new javax.swing.JLabel();
         jLabelBNombre2 = new javax.swing.JLabel();
+        jLabelBNombre3 = new javax.swing.JLabel();
+        jLabelBNombre4 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
         jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(612, 406));
         setMinimumSize(new java.awt.Dimension(612, 406));
+        setSize(new java.awt.Dimension(612, 406));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setMaximumSize(new java.awt.Dimension(612, 406));
         jPanel1.setPreferredSize(new java.awt.Dimension(612, 406));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel4.setBackground(new java.awt.Color(255, 255, 255));
+        jTextField2.setBackground(new java.awt.Color(0, 0, 0));
+        jTextField2.setForeground(new java.awt.Color(255, 153, 0));
+        jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 70, 120, -1));
+
+        jLabel4.setBackground(new java.awt.Color(0, 0, 0));
         jLabel4.setFont(new java.awt.Font("Cambria", 1, 36)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 102, 0));
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel4.setText("CHECK-IN HOTEL BOOKING");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 10, -1, -1));
+        jLabel4.setOpaque(true);
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -7, 620, 70));
 
-        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 102, 0));
-        jLabel2.setText("Ingrese le cedule de le persone:");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 70, -1, -1));
-        jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 70, 120, -1));
-
-        jButton1.setText("Buscar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 70, -1, -1));
-
+        jButton2.setBackground(new java.awt.Color(0, 0, 0));
+        jButton2.setFont(new java.awt.Font("Arial Black", 1, 14)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 153, 0));
         jButton2.setText("Volver");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 370, -1, -1));
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 370, -1, -1));
+
+        jButton1.setBackground(new java.awt.Color(0, 0, 0));
+        jButton1.setFont(new java.awt.Font("Arial Black", 1, 14)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 153, 0));
+        jButton1.setText("Buscar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 70, -1, -1));
 
         jLabelBNombre.setBackground(new java.awt.Color(255, 255, 255));
         jLabelBNombre.setForeground(new java.awt.Color(0, 0, 0));
-        jLabelBNombre.setOpaque(true);
-        jPanel1.add(jLabelBNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 130, 90, 30));
+        jPanel1.add(jLabelBNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 100, 340, 20));
 
         jLabelBApellido.setBackground(new java.awt.Color(255, 255, 255));
         jLabelBApellido.setForeground(new java.awt.Color(0, 0, 0));
-        jLabelBApellido.setOpaque(true);
-        jPanel1.add(jLabelBApellido, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 170, 120, 30));
+        jPanel1.add(jLabelBApellido, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 130, 340, 20));
 
         jLabelBEmail.setBackground(new java.awt.Color(255, 255, 255));
         jLabelBEmail.setForeground(new java.awt.Color(0, 0, 0));
-        jLabelBEmail.setOpaque(true);
-        jPanel1.add(jLabelBEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 210, 290, 30));
+        jPanel1.add(jLabelBEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 160, 360, 20));
 
         jLabelBGenero.setBackground(new java.awt.Color(255, 255, 255));
         jLabelBGenero.setForeground(new java.awt.Color(0, 0, 0));
-        jLabelBGenero.setOpaque(true);
-        jPanel1.add(jLabelBGenero, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 250, 120, 30));
+        jPanel1.add(jLabelBGenero, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 190, 340, 20));
 
         jLabelBNombre1.setBackground(new java.awt.Color(255, 255, 255));
         jLabelBNombre1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabelBNombre1.setOpaque(true);
-        jPanel1.add(jLabelBNombre1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 290, 90, 30));
+        jPanel1.add(jLabelBNombre1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 220, 220, 20));
 
         jLabelBNombre2.setBackground(new java.awt.Color(255, 255, 255));
         jLabelBNombre2.setForeground(new java.awt.Color(0, 0, 0));
-        jLabelBNombre2.setOpaque(true);
-        jPanel1.add(jLabelBNombre2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 330, 90, 30));
+        jPanel1.add(jLabelBNombre2, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 250, 240, 20));
 
-        jLabel3.setText("Nombre");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 140, -1, -1));
+        jLabelBNombre3.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelBNombre3.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel1.add(jLabelBNombre3, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 280, 230, 20));
+
+        jLabelBNombre4.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelBNombre4.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel1.add(jLabelBNombre4, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 310, 240, 20));
+
+        jPanel1.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 340, 100, 30));
+
+        jLabel3.setBackground(new java.awt.Color(255, 153, 0));
+        jLabel3.setFont(new java.awt.Font("Arial", 0, 25)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel3.setText("<html>\nIngrese la cedula del cliente:<br>\nNombre:<br>\nApellido:<br>\nEmail:<br>\nGenero:<br>\nTipo de Habitacion:<br>\nTelefono Celular:<br>\nFecha de Llegada:<br>\nFecha de Salida:\n<html>\n");
+        jLabel3.setOpaque(true);
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 60, 620, 280));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyecto2eddandresimerycarlosgonzalez/fondorecepcion.jpg"))); // NOI18N
         jLabel1.setOpaque(true);
@@ -166,8 +183,8 @@ public class BuscarReserva extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabelBApellido;
@@ -176,6 +193,8 @@ public class BuscarReserva extends javax.swing.JFrame {
     private javax.swing.JLabel jLabelBNombre;
     private javax.swing.JLabel jLabelBNombre1;
     private javax.swing.JLabel jLabelBNombre2;
+    private javax.swing.JLabel jLabelBNombre3;
+    private javax.swing.JLabel jLabelBNombre4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
